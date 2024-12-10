744fd2f73f166ae66c130683dba33cd63e954162
Reinis <rrruza@gmail.com>
2024-12-10 20:46:47 +0200

    Deployment_test

--- test.py
+++ test.py
@@ -1,41 +1,35 @@
-import httpx
-
-from prefect import flow, task # Prefect flow and task decorators
+import time
+from prefect import flow, task
 
 
 @flow(log_prints=True)
-def show_stars(github_repos: list[str]):
-    """Flow: Show the number of stars that GitHub repos have"""
+def download_files():
 
-    for repo in github_repos:
-        # Call Task 1
-        repo_stats = fetch_stats(repo)
+    time.sleep(3)
 
-        # Call Task 2
-        stars = get_stars(repo_stats)
+    print(f"Fetching filelist")
+    get_filelist()
+    
+    print(f"Downloading data")
+    download_data()
 
-        # Print the result
-        print(f"{repo}: {stars} stars")
-
-
-@task
-def fetch_stats(github_repo: str):
-    """Task 1: Fetch the statistics for a GitHub repo"""
-
-    return httpx.get(f"https://api.github.com/repos/{github_repo}").json()
+    print(f"Writing files")
+    write_files()
+    
+    print(f"All files have been downloaded")
 
 
 @task
-def get_stars(repo_stats: dict):
-    """Task 2: Get the number of stars from GitHub repo statistics"""
+def get_filelist():
+    time.sleep(6)
 
-    return repo_stats['stargazers_count']
+@task
+def download_data():
+    time.sleep(8)
 
+@task
+def write_files():
+    time.sleep(3)
 
-# Run the flow
 if __name__ == "__main__":
-    show_stars([
-        "PrefectHQ/prefect",
-        "pydantic/pydantic",
-        "huggingface/transformers"
-    ])
+    download_files()
